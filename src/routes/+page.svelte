3e3e6c8d381baf3c95fa362f13ab66a01d897b45
Jackson Choyce <jacksonchoyce@gmail.com>
2024-08-30 16:54:52 -0500

    add funny cube

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,39 @@
 <script lang="ts">
 	import ProjectItem from '$lib/components/ProjectItem.svelte';
+	import * as THREE from 'three';
+	import * as SC from 'svelte-cubed';
+	import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry.js';
 
+	const dimensions = 3;
 	const timeZone = 'America/Chicago';
 	const isTimeZoneSame = Intl.DateTimeFormat().resolvedOptions().timeZone === timeZone;
+
 	let timeZoneToggle = false;
+	let cubes: { position: [number, number, number] }[] = [];
+
+	const offset = (dimensions - 1) / 2;
+
+	// Generate cube positions
+	cubes = Array.from({ length: dimensions ** 3 }, (_, i) => {
+		const x = i % dimensions;
+		const y = Math.floor((i / dimensions) % dimensions);
+		const z = Math.floor(i / dimensions ** 2);
+		return {
+			position: [x - offset, y - offset, z - offset]
+		};
+	});
+
+	const loader = new THREE.CubeTextureLoader();
+	loader.setPath('https://threejs.org/examples/textures/cube/MilkyWay/');
+
+	const envMap = loader.load([
+		'dark-s_px.jpg',
+		'dark-s_nx.jpg',
+		'dark-s_py.jpg',
+		'dark-s_ny.jpg',
+		'dark-s_pz.jpg',
+		'dark-s_nz.jpg'
+	]);
 
 	$: timeFormatter = new Intl.DateTimeFormat('en-US', {
 		hour: 'numeric',
@@ -72,3 +102,51 @@
 		{/if}
 	</div>
 </section>
+
+<SC.Canvas antialias alpha shadows>
+	{#each cubes as cube}
+		<SC.Mesh
+			geometry={new RoundedBoxGeometry(0.95, 0.95, 0.95, 8, 0.05)}
+			material={new THREE.MeshPhysicalMaterial({
+				color: 'black',
+				metalness: 0.8,
+				roughness: 0.1,
+				clearcoat: 1,
+				clearcoatRoughness: 0.05,
+				envMap: envMap
+			})}
+			position={cube.position}
+			castShadow
+			receiveShadow
+		/>
+	{/each}
+
+	<SC.PerspectiveCamera position={[5, 5, 5]} />
+	<SC.AmbientLight intensity={0.4} />
+	<SC.DirectionalLight intensity={0.6} position={[10, 10, 10]} />
+	<SC.OrbitControls
+		enablePan={false}
+		enableZoom={false}
+		maxPolarAngle={Math.PI * 0.8}
+		autoRotate
+		autoRotateSpeed={0.5}
+		enableDamping
+		dampingFactor={0.1}
+	/>
+</SC.Canvas>
+
+<style>
+	:global(canvas) {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100vh;
+		background: transparent;
+		z-index: 1;
+	}
+	section {
+		position: relative;
+		z-index: 2;
+	}
+</style>
